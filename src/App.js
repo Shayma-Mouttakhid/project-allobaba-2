fd9548bffcde1e86ce703ed00b9e3b6b295bedf9
Shayma-Mouttakhid <businesschama93@gmail.com>
2024-03-06 23:32:33 +0100

    the first commit sh

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState } from "react";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Service from './components/services/Services';
+import Progressbar from './components/ProgressBar/Progressbar';
 
 function App() {
+  const [currentStep, setCurrentStep] = useState(1);
+  const [complete, setComplete] = useState(false);
   return (
     <div className="App">
-      
+      <Progressbar currentStep={currentStep} complete={complete}/>
+      <Service setCurrentStep={setCurrentStep} setComplete={setComplete}/>
     </div>
   );
 }
---
+++ src/components/services/Services.jsx
@@ -0,0 +1,38 @@
+// Services.js
+import React from "react";
+import "./Services.css";
+export default function Services({ setCurrentStep, setComplete }) {
+  const Services = [
+    { id: 1, name: "Marketing Digitale", img: "/images/marketing.jpg" },
+    { id: 2, name: "Développement web/mobile", img: "/images/web.jpg" },
+    { id: 3, name: "Audio-visuel", img: "/images/audiov.jpg" },
+    { id: 4, name: "Design/Infographie", img: "/images/design.jpg" },
+    { id: 5, name: "Montage des vidéos", img: "./images/montage.jpg" },
+    { id: 6, name: "Formation/Coaching", img: "./images/coaching.jpg" }
+  ];
+
+  const handleSave = () => {
+    setCurrentStep(2);
+    
+  };
+
+  return (
+    <div className="container">
+      <h1 className="text-center m-5">Our Services</h1>
+      <div className="row">
+        {Services.map((service) => (
+          <div key={service.id} className="col-md-4 mb-4">
+            <div className="card">
+              <img className="card-img-top" src={service.img} alt={service.name} />
+              <div className="card-body">
+                <h2 className="card-title">{service.name}</h2>
+                <p className="card-text">{service.desc}</p>
+                <button onClick={handleSave} className="btn">Save</button>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
