ac9467a79b3a74403a7896c3e60a68d549cc013d
Shayma-Mouttakhid <businesschama93@gmail.com>
2024-03-20 13:55:27 +0000

    correct the merging code

--- src/components/ClientSubCategory/ClientSubCategory.jsx
+++ src/components/ClientSubCategory/ClientSubCategory.jsx
@@ -1,18 +1,14 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import { useNavigate } from 'react-router-dom';
-import "./ClientSubCategory.css";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-import "../Style/style.css";
-
+import "../Style/style.css"
 export default function ClientSubCategory({ setCurrentStep }) {
-    useEffect(() => {
-        setCurrentStep(3);
-    }, [setCurrentStep]);
-
     const selectedCategory = localStorage.getItem('selectedCategory');
     const Client = JSON.parse(localStorage.getItem('StoredClient'));
+
+    setCurrentStep(3);
     const navigate = useNavigate();
 
     const handleChooseSubCategory = (subcategory) => {
@@ -22,18 +18,17 @@ export default function ClientSubCategory({ setCurrentStep }) {
         } else if (subcategory === "Auto-entrepreneur" || subcategory === "Offline Busines" || subcategory === "Profession libérale") {
             navigate('/sectors');
         } else if (subcategory === "Questions/réponses") {
-            navigate('/questionForm');
+            navigate('/questionForm')
         } else {
             localStorage.setItem('SelectedSubCategory', JSON.stringify(subcategory));
             navigate('/Infos');
         }
+        console.log(subcategory);
     };
-
     const handleGoBack = () => {
         setCurrentStep(2);
         navigate('/clientCategory');
     };
-
     const settings = {
         dots: false,
         infinite: true,
@@ -41,7 +36,7 @@ export default function ClientSubCategory({ setCurrentStep }) {
         slidesToScroll: 1,
         centerMode: true,
         focusOnSelect: true,
-        autoplay: true,
+        autoplay: true, // Autoplay slides
         autoplaySpeed: 1500,
         arrows: false,
     };
@@ -49,25 +44,49 @@ export default function ClientSubCategory({ setCurrentStep }) {
     return (
         <div className="container-fluid">
             <h1 className="text-center text-dark m-5">Quelle est votre sous catégorie?</h1>
-            {Client && Client.map((clientItem, index) => (
-                clientItem.category === selectedCategory && (
-                    <div key={index} className="row justify-content-center">
-                        {clientItem.subcategory.map((subcategory, subIndex) => (
-                            <div key={subIndex} className="col-md-3 mb-4">
-                                <div className="card" style={{ backgroundImage: `url(${subcategory.img})` }}>
-                                    <div className="card-overlay">
-                                        <h2 className="card-title">{subcategory.title}</h2>
-                                        <button className="btn" onClick={() => handleChooseSubCategory(subcategory.title)}>Choisir</button>
+            <div className="d-none d-md-block">
+                {Client.map((clientItem, index) => (
+                    clientItem.category === selectedCategory && (
+                        <div key={index} className="row justify-content-center">
+                            {clientItem.subcategory.map((subcategory, subIndex) => (
+                                <div key={subIndex} className="col-md-3 mb-4">
+                                    <div className="card" style={{ backgroundImage: `url(${subcategory.img})` }}>
+                                        <div className="card-overlay">
+                                            <h2 className="card-title">{subcategory.title}</h2>
+                                            <button className="btn" onClick={() => handleChooseSubCategory(subcategory.title)}>Choisir</button>
+                                        </div>
                                     </div>
                                 </div>
+                            ))}
+                        </div>
+                    )
+                ))}
+            </div>
+            <div className="d-block d-md-none">
+               
+                    {Client.map((clientItem, index) => (
+                        clientItem.category === selectedCategory && (
+                            <div key={index} className="row justify-content-center">
+                                 <Slider {...settings}>
+                                {clientItem.subcategory.map((subcategory, subIndex) => (
+                                    <div  key={subIndex} className="col-md-3 mb-4"  >
+                                        <div key={subIndex} className="card" style={{ backgroundImage: `url(${subcategory.img})` }}>
+                                            <div className="card-overlay">
+                                                <h2  className="card-title" >{subcategory.title}</h2>
+                                                <button className="btn" onClick={() => handleChooseSubCategory(subcategory.title)}>Choisir</button>
+                                            </div>
+                                        </div>
+                                    </div>
+                                ))} </Slider>
                             </div>
-                        ))}
-                    </div>
-                )
-            ))}
-            <div className="text-center">
+                        )
+                    ))}
+               
+            </div>
+            <div className="text-center ">
                 <button className="return-button" onClick={handleGoBack}>Retour</button>
             </div>
         </div>
     );
+
 }
